09843a27d65ea4fd72759b6257c196fbe14298ce
Sherif Nafee <shico.m.nafee@gmail.com>
2021-09-28 22:29:36 +0300

    Refactor Tmotor

--- src/Tmotor.py
+++ src/Tmotor.py
@@ -1,45 +1,61 @@
-from CANSocket import CANDevice
+from abc import ABC
+from dataclasses import dataclass
+from CANSocket import CANSocket
+import builtins
 
 
-class TMotorQDD:
+def clamp(value: float, min: float, max: float):
+    return builtins.max(min, builtins.min(max, value))
+
+
+# @dataclass
+class MotorState:
+    """Represents the current state of the motor."""
+
+    MIN_POSITION = -18
+    MAX_POSITION = 18
+    # position: float = 0
+    # velocity: float = 0
+    # torque: float = 0
+
+    def __init__(self, position: float) -> None:
+        self.position = clamp(
+            position, MotorState.MIN_POSITION, MotorState.MAX_POSITION
+        )
+
+
+@dataclass
+class ControllerState:
+    kp: float
+    kd: float
+
+
+class Frame:
+    def __init__(self, state: MotorState, min_state: MotorState):
+        self.state = state
+
+
+class TMotor(ABC):
+    pass
+
+
+class TMotorQDD(TMotor):
     # TODO:
     # add tx_message dict
     # add rx_message dict
-    def __init__(self, can_socket=None, device_id=0x001, rxtx_interface=None):
+    def __init__(self, can_socket: CANSocket, device_id=0x001, rxtx_interface=None):
 
         # TODO: pass dict with reciver/transmitter functions from the specific bus
-        if not can_socket:
-            print("Provide can_bus as argument")
-            self.__del__()
+        if not isinstance(can_socket, CANSocket):
+            raise TypeError("can_socket is not an instance of CANSocket")
 
         self.setup_communication(can_socket)
 
         # TODO: Implement motoring name addressing
         self.name = None
 
-        self.set_state_settings()
-
-        self.bounds = dict(zip(self.state_send, self.state_bounds))
-        self.device_id = device_id
-
-        self.commands = {
-            "mot_mode_on": b"\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFC",
-            "mot_mode_off": b"\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFD",
-            "set_zero": b"\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFE",
-            "empty": b"\x00\x00\x00\x00\x00\x00\x00\x00",
-            "zero_state": self.zero_state_bytes,
-        }
-
-        self.t_data = self.commands["mot_mode_off"]
-        self.r_data = self.commands["empty"]
-        self.torque_limit = 1
-        self.gear_ratio = 6
-        self.torque_constant = 1
-        self.message = {self.device_id: self.commands["zero_state"]}
-
-    def set_state_settings(self):
         self.state_send = ["pos", "vel", "kp", "kd", "tor"]
-        self.state_bounds = [
+        state_bounds = [
             [-95.5, 95.5],
             [-30.0, 30.0],
             [0.0, 500.0],
@@ -49,28 +65,43 @@ class TMotorQDD:
         self.state_bits = dict(zip(self.state_send, [16, 12, 12, 12, 12]))
         self.des_state_int = dict(zip(self.state_send, [0, 0, 0, 0, 0]))
         self.zero_state = dict(zip(self.state_send, [0.0, 0.0, 0.0, 0.0, 0.0]))
-        self.zero_state_bytes = self.state_to_bytes(self.zero_state)
         self.states_recv = ["pos", "vel", "tor"]
         self.state_recv_bytes = dict(zip(self.states_recv, [b"\x00", b"\x00", b"\x00"]))
-        self.state_recv_ints = dict(zip(self.states_recv, [0, 0, 0]))
-        self.state = dict(zip(self.states_recv, [0.0, 0.0, 0.0]))
+        self.state = dict(zip(self.states_recv, [0.0, 0.0, 0.0]))  # MotorState()
 
+        self.bounds = dict(zip(self.state_send, state_bounds))
+        zero_state_bytes = self.state_to_bytes(self.zero_state)
+        self.device_id = device_id
+
+        self.COMMANDS = {
+            "mot_mode_on": bytes([255, 255, 255, 255, 255, 255, 255, 252]),
+            "mot_mode_off": bytes([255, 255, 255, 255, 255, 255, 255, 253]),
+            "set_zero": bytes([255, 255, 255, 255, 255, 255, 255, 254]),
+            "empty": bytes([0, 0, 0, 0, 0, 0, 0, 0]),
+            "zero_state": zero_state_bytes,
+        }
+        self.torque_limit = 1
+
+    # here we can use an instance of CANSocket
+    # insted of making this assignment
     def setup_communication(self, can_bus):
-        self.transmiter = can_bus.send_bytes
-        self.reciver = can_bus.recive_frame
+        self.transmitter = can_bus.send_bytes
+        self.reciever = can_bus.recive_frame
 
     def __del__(self):
         print("Motor object was destructed")
 
-    def float_to_uint(self, data_float, data_min, data_max, bits):
+    def float_to_uint(
+        self, data_float: float, data_min: float, data_max: float, bits: int
+    ) -> int:
         span = data_max - data_min
-        offset = data_min
-        return int((data_float - offset) * (float((1 << bits) - 1)) / span)
+        return int((data_float - data_min) * (float((1 << bits) - 1)) / span)
 
-    def uint_to_float(self, data_int, data_min, data_max, bits):
+    def uint_to_float(
+        self, data_int: int, data_min: int, data_max: int, bits: int
+    ) -> float:
         span = data_max - data_min
-        offset = data_min
-        return (float(data_int)) * span / (float((1 << bits) - 1)) + offset
+        return float(data_int) * span / float((1 << bits) - 1) + data_min
 
     def state_to_bytes(self, state_dict):
         for state_label in self.state_send:
@@ -104,6 +135,12 @@ class TMotorQDD:
                 (recived_bytes[4] & 0xF) << 8
             ) | recived_bytes[5]
 
+            # self.state.position = self.uint_to_float(
+            #     self.state_recv_bytes['position'],
+            #     self.bounds['position'][0],
+            #     self.bounds['position'][1],
+            #     self.state_bits['position'],
+            # )
             for state_label in self.states_recv:
                 self.state[state_label] = self.uint_to_float(
                     self.state_recv_bytes[state_label],
@@ -114,31 +151,36 @@ class TMotorQDD:
         else:
             pass
 
+    # ============================================================================================
+    # maybe change this somehow to use CANSocket also
     def send_command(self, command):
-        self.transmiter(self.device_id, command)
+        self.can_bus.send_bytes(self.device_id, command)
 
     def recive_reply(self):
-        _, _, self.reply = self.reciver()
+        _, _, self.reply = self.reciever()
         return self.reply
 
     def enable(self):
-        self.send_command(self.commands["mot_mode_on"])
+        self.send_command(self.COMMANDS["mot_mode_on"])
         print("Motor mode enabled")
 
     def disable(self):
-        self.send_command(self.commands["mot_mode_off"])
+        self.send_command(self.COMMANDS["mot_mode_off"])
         print("Motor mode disabled")
 
+    # Edit this function to be compatable with our Client-Server archeticture
     def set_zero(self):
         print(
             f'You are going to assign a new zero for motor with ID {self.device_id}, press "Y" to continue...\n'
         )
         user_input = input()
         if user_input == "Y" or user_input == "y":
-            self.send_command(self.commands["set_zero"])
+            self.send_command(self.COMMANDS["set_zero"])
             print("New encoder zero is setted")
         else:
             print("Canceling...")
+
+    # =============================================================================================
 
     def set_torque(self, torque):
         if torque > self.torque_limit:
@@ -146,7 +188,7 @@ class TMotorQDD:
         if torque < -self.torque_limit:
             torque = -self.torque_limit
 
-        state_data_dict = self.zero_state
+        state_data_dict = self.zero_state.copy()
         state_data_dict["tor"] = torque
         self.send_command(self.state_to_bytes(state_data_dict))
         self.recive_reply()
@@ -154,7 +196,7 @@ class TMotorQDD:
 
     def set_torque_limit(self, torque_limit):
         self.torque_limit = torque_limit
-        print(f"Torque limit is seted to: {torque_limit}")
+        print(f"Torque limit is set to: {torque_limit}")
 
     def set_state(self, state_data_dict):
         self.send_command(self.state_to_bytes(state_data_dict))
